96b433e25496eb97e100c98cbed6d9ec129f6f49
rollydelosreyes02 <rolly.a.delosreyes@gmail.com>
2024-06-19 10:17:49 +0800

    fixed and now running :)

--- src/main/java/com/crud/thymeleaf/ThymeleafApplication.java
+++ src/main/java/com/crud/thymeleaf/ThymeleafApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class thymeleafApplication {
+public class ThymeleafApplication {
    public static void main(String[] args) {
-      SpringApplication.run(thymeleafApplication.class, args);
+      SpringApplication.run(ThymeleafApplication.class, args);
    }
 }
